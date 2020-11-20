931d60c8c4cf4b54eb9ef784439f581857e92c7a
gure-y <y.8kp.gm@gmail.com>
2020-11-20 20:03:34 +0900

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => 'api'], function(){
-    Route::get('get', [TodoController::class, 'getTodos']);
+    Route::get('get', 'TodoController@getTodos');
 });
